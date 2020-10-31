855dd63d93845d25855e28d750162e543c45b38d
brunao <brunoborges158@yahoo.com.br>
2020-10-31 10:48:02 -0300

    first commit

---
+++ app/src/main/java/baborges45/metodo52semanas/MainActivity.kt
@@ -0,0 +1,11 @@
+package baborges45.metodo52semanas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
